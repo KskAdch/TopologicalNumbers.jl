afb5379960ff42f15ad32cf4061985c815c6eaab
Documenter.jl <documenter@juliadocs.github.io>
2023-07-13 15:35:31 +0000

    build based on c3b4bd6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TopologicalNumbers","category":"page"},{"location":"#TopologicalNumbers","page":"Home","title":"TopologicalNumbers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TopologicalNumbers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TopologicalNumbers]","category":"page"}]
+}
