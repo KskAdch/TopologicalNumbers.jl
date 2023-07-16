828c58e4821bee6b74740c903604bd333879e0ca
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2023-07-17 02:42:32 +0900

    Bug fixes

--- docs/make.jl
+++ docs/make.jl
@@ -5,21 +5,21 @@ DocMeta.setdocmeta!(TopologicalNumbers, :DocTestSetup, :(using TopologicalNumber
 
 makedocs(;
     modules=[TopologicalNumbers],
-    authors="Keisuke Adachi, Minoru Kanega",
+    authors="Keisuke Adachi <18s2002x@gmail.com>", "Minoru Kanega <phys_chibaraki@yahoo.co.jp>",
     repo="https://github.com/KskAdch/TopologicalNumbers.jl/blob/{commit}{path}#{line}",
     sitename="TopologicalNumbers.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://KskAdch.github.io/TopologicalNumbers.jl",
         edit_link="main",
-        assets=String[],
+        assets=String[]
     ),
     pages=[
         "Home" => "index.md",
-    ],
+    ]
 )
 
 deploydocs(;
     repo="github.com/KskAdch/TopologicalNumbers.jl",
-    devbranch="main",
+    devbranch="main"
 )
