af3f16bc576fdd14e2b2bd6601b11f2563b8d04e
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2023-07-17 03:04:36 +0900

    docs: fix authors format

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ DocMeta.setdocmeta!(TopologicalNumbers, :DocTestSetup, :(using TopologicalNumber
 
 makedocs(;
     modules=[TopologicalNumbers],
-    authors="Keisuke Adachi <18s2002x@gmail.com>", "Minoru Kanega <phys_chibaraki@yahoo.co.jp>",
+    authors="Keisuke Adachi <18s2002x@gmail.com>, Minoru Kanega <phys_chibaraki@yahoo.co.jp>",
     repo="https://github.com/KskAdch/TopologicalNumbers.jl/blob/{commit}{path}#{line}",
     sitename="TopologicalNumbers.jl",
     format=Documenter.HTML(;
