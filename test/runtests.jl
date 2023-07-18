930494535405138c91a1d4c6159dbaef7da19892
Keisuke Adachi <18s2002x@gmail.com>
2023-07-18 20:20:24 +0900

    add test Z2Number

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,29 +15,56 @@ using Test
     end
 
     @testset "2D case" begin
-        function H(k) # landau
-            k1, k2 = k
-            t = 1
-        
-            Hsize = 6
-            Hmat = zeros(ComplexF64, Hsize, Hsize)
-        
-            for i in 1:Hsize
-                Hmat[i, i] = -2*cos(k2-2pi*i/Hsize)
-            end
-        
-            for i in 1:Hsize-1
-                Hmat[i, i+1] = -t
-                Hmat[i+1, i] = -t
-            end
-        
-            Hmat[1, Hsize] = -t*exp(-im*k1)
-            Hmat[Hsize, 1] = -t*exp(im*k1)
+
+        @testet "FirstChern" begin
+            function H(k) # landau
+                k1, k2 = k
+                t = 1
             
-            Hmat
+                Hsize = 6
+                Hmat = zeros(ComplexF64, Hsize, Hsize)
+            
+                for i in 1:Hsize
+                    Hmat[i, i] = -2*cos(k2-2pi*i/Hsize)
+                end
+            
+                for i in 1:Hsize-1
+                    Hmat[i, i+1] = -t
+                    Hmat[i+1, i] = -t
+                end
+            
+                Hmat[1, Hsize] = -t*exp(-im*k1)
+                Hmat[Hsize, 1] = -t*exp(im*k1)
+                
+                Hmat
+            end
+
+            @test FirstChern(H) == (TopologicalNumber = [1, 1, -2, -2, 1, 1], Total = 0)
         end
 
-        @test FirstChern(H) == (TopologicalNumber = [1, 1, -2, -2, 1, 1], Total = 0)
+        @testset "Z2Number" begin
+            function H(k) # 2d Kane-Mele
+                k1, k2 = k
+                t = 1
+
+                R3 = 2(sin(k1) - sin(k2) - sin(k1-k2))
+                R4 = -t*(sin(k1) + sin(k2))
+                R5 = -t*(cos(k1) + cos(k2) + 1)
+
+                s0 = [1 0; 0 1]
+                sx = [0 1; 1 0]
+                sy = [0 -im; im 0]
+                sz = [1 0; 0 -1]
+
+                a3 = kron(sz, sz)
+                a4 = kron(sy, s0)
+                a5 = kron(sx, s0)
+
+                R3*a3+R4*a4+R5*a5
+            end
+
+            @test Z2Invariants2D(H) == (TopologicalNumber = [1 1 1 1; 1 1 1 1], Total = 0)
+        end
 
     end
 
