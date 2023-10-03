98a0887a055958f4406726560a0d55ab7c36a3fb
Keisuke Adachi <139373570+KskAdch@users.noreply.github.com>
2023-10-03 20:43:26 +0900

    Update calcBerryFlux.jl (docs bug fixed)

--- src/calcBerryFlux.jl
+++ src/calcBerryFlux.jl
@@ -90,11 +90,11 @@ end
  The Berry flux at the wavenumber $\bm{k}$ of the $n$th band $F_{n}(\bm{k})$ is defined by
 ```math
 F_{n}(\bm{k})=f_{n}(\bm{k})-df_{n}(\bm{k})
-'''
-'''math
+```
+```math
 f_{n}(\bm{k})=\frac{1}{2\pi}\mathrm{Im}\mathrm{Log}\left[U_{n,1}(\bm{k})U_{n,2}(\bm{k}+\bm{e}_{1})U_{n,1}^{*}(\bm{k}+\bm{e}_{2})U_{n,1}^{*}(\bm{k})\right]
-'''
-'''math
+```
+```math
 df_{n}(\bm{k})=\frac{1}{2\pi}\mathrm{Im}\left(\mathrm{Log}U_{n,1}(\bm{k})+\mathrm{Log}U_{n,2}(\bm{k}+\bm{e}_{1})-\mathrm{Log}U_{n,1}(\bm{k}+\bm{e}_{2})-\matthrm{Log}U_{n,1}(\bm{k})\right)
 ```
  $U_{n,i}(\bm{k})$ is the link variable at wavenumber $\bm{k}$. $\bm{e}_{i}$ is the unit vector.
