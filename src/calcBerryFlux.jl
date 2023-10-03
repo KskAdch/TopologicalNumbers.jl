d8b7a02dcf41f188158d7dd56f712d7fc6c331f6
Keisuke Adachi <139373570+KskAdch@users.noreply.github.com>
2023-10-03 21:09:28 +0900

    Update calcBerryFlux.jl

--- src/calcBerryFlux.jl
+++ src/calcBerryFlux.jl
@@ -92,10 +92,10 @@ end
 F_{n}(\bm{k})=f_{n}(\bm{k})-df_{n}(\bm{k})
 ```
 ```math
-f_{n}(\bm{k})=\frac{1}{2\pi}\mathrm{Im}\mathrm{Log}\left[U_{n,1}(\bm{k})U_{n,2}(\bm{k}+\bm{e}_{1})U_{n,1}^{*}(\bm{k}+\bm{e}_{2})U_{n,1}^{*}(\bm{k})\right]
+f_{n}(\bm{k})=\frac{1}{2\pi}\mathrm{Im}\left[\mathrm{Log}\left[U_{n,1}(\bm{k})U_{n,2}(\bm{k}+\bm{e}_{1})U_{n,1}^{*}(\bm{k}+\bm{e}_{2})U_{n,1}^{*}(\bm{k})\right]\right]
 ```
 ```math
-df_{n}(\bm{k})=\frac{1}{2\pi}\mathrm{Im}\left(\mathrm{Log}U_{n,1}(\bm{k})+\mathrm{Log}U_{n,2}(\bm{k}+\bm{e}_{1})-\mathrm{Log}U_{n,1}(\bm{k}+\bm{e}_{2})-\matthrm{Log}U_{n,1}(\bm{k})\right)
+df_{n}(\bm{k})=\frac{1}{2\pi}\mathrm{Im}\left[\left(\mathrm{Log}U_{n,1}(\bm{k})+\mathrm{Log}U_{n,2}(\bm{k}+\bm{e}_{1})-\mathrm{Log}U_{n,1}(\bm{k}+\bm{e}_{2})-\mathrm{Log}U_{n,1}(\bm{k})\right)\right]
 ```
  $U_{n,i}(\bm{k})$ is the link variable at wavenumber $\bm{k}$. $\bm{e}_{i}$ is the unit vector.
 ```math
