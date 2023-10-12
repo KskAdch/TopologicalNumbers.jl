5b3c62c25a26bdf3cfbea35820f7e477382ddad9
Keisuke Adachi <18s2002x@gmail.com>
2023-10-12 23:53:21 +0900

    test revised

--- test/runtests.jl
+++ test/runtests.jl
@@ -387,13 +387,13 @@ Aqua.test_all(TopologicalNumbers; ambiguities=false)
         @test calcWeylNode(H₀, [3, 10, 0]; N=11) == (TopologicalNumber=[1, 0], n=[3, 10, 0])
         # @test calcWeylNode(H₀, [3, 10, 0]; N=11, rounds=false) == (TopologicalNumber=[1.0, -2.220446049250313e-16], n=[3, 10, 0])
 
-        N = 10
+        N = 8
         nodes = zeros(N, N, N, 2)
         for i in 1:N, j in 1:N, k in 1:N
             nodes[i, j, k, :] = calcWeylNode(H₀, [i-1, j-1, k-1]; N=N, rounds=false).TopologicalNumber
         end
         Chern_i = [[round(Int, sum(nodes[i, :, :, 1])) for i in 1:N] [round(Int, sum(nodes[i, :, :, 2])) for i in 1:N]]
-        @test Chern_i[:, 1] == [0, 0, 1, 0, 0, 0, 0, -1, 0, 0]
+        @test Chern_i[:, 1] == [0, 0, 1, 0, 0, -1, 0, 0]
         @test Chern_i[:, 1] == -Chern_i[:, 2]
         # N = 6
         # nodes = zeros(N, N, N, 2)
