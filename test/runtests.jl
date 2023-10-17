2d236932aa3cdd5891225c5f9af478c167d6c723
Keisuke Adachi <18s2002x@gmail.com>
2023-10-17 21:40:27 +0900

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -149,14 +149,14 @@ Aqua.test_all(TopologicalNumbers; ambiguities=false)
                 H(k, p) = H₀(k, (6, p))
 
                 param = 1:6 # range(1, 6, length=6)
-                result = calcPhaseDiagram(H, param, "Chern")
+                result = calcPhaseDiagram(H, param, "Chern"; rounds=false)
 
                 # #↓結果おかしい
                 # num = [6 6 -12 -12 6 6; 0 6 -6 -6 6 0; 0 4 -2 -1 1 0; 0 -6 6 6 -6 0; -6 -6 12 12 -6 -6; 0 2 -6 1 0 0]
                 # num = [1 1 -2 -2 1 1; 0 1 -1 -1 1 0; 0 3 -3 0 -1 0; 0 -1 1 1 -1 0; -1 -1 2 2 -1 -1; 0 -2 4 -1 2 0]
                 @test result.nums[1, :] == [1, 1, -2, -2, 1, 1]
                 @test result.nums[2, :] == [0, 1, -1, -1, 1, 0]
-                # @test result.nums[3, :] == [0, 3, -3, 0, -1, 0]
+                @test result.nums[3, :] == [0, 3, -3, 0, -1, 0]
                 @test result.nums[4, :] == [0, -1, 1, 1, -1, 0]
                 @test result.nums[5, :] == [-1, -1, 2, 2, -1, -1]
                 # @test result.nums[6, :] == [0, -2, 4, -1, 2, 0]
