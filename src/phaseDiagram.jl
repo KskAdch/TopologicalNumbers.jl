0457e1abb44f2968e06497f3708b3cfb5b6d2b36
Keisuke Adachi <18s2002x@gmail.com>
2023-10-20 22:27:42 +0900

    update phasediagram

--- src/phaseDiagram.jl
+++ src/phaseDiagram.jl
@@ -96,48 +96,82 @@ function calc_data1D(H, param_range, alg, p::Params)
     end
 end
 
-# function calc_data2D(H, param_range1, param_range2, alg, p::Params)
-#     @unpack rounds, Hs = p
+function calc_data2D(H, param_range1, param_range2, alg, p::Params)
+    @unpack rounds, Hs = p
 
-#     nums = zeros(Float64, Hs, size(param_range1, 1), size(param_range2, 1))
+    nums = zeros(Float64, Hs, size(param_range1, 1), size(param_range2, 1))
+    num0 = zeros(Float64, Hs)
 
-#     if rounds == true
-#         num0 = zeros(Int64, Hs)
+    if rounds == true
+        num0 = zeros(Int64, Hs)
 
-#         if alg == "BerryPhase"
-#             algorithm! = BerryPhase_round!
-#         elseif alg == "Z2"
-#             algorithm! = Z2Phase_round!
-#             nums = zeros(Float64, Hs ÷ 2, size(param_range1, 1), size(param_range2, 1))
-#             num0 = zeros(Int64, Hs ÷ 2)
-#         elseif alg == "Chern"
-#             algorithm! = ChernPhase!
-#         else
-#             throw(ArgumentError("Unknown algorithm $alg"))
-#         end
+    #     if alg == "BerryPhase"
+    #         algorithm! = BerryPhase_round!
+    #     elseif alg == "Z2"
+    #         algorithm! = Z2Phase_round!
+    #         nums = zeros(Float64, Hs ÷ 2, size(param_range1, 1), size(param_range2, 1))
+    #         num0 = zeros(Int64, Hs ÷ 2)
+    #     elseif alg == "Chern"
+    #         algorithm! = ChernPhase!
+    #     else
+    #         throw(ArgumentError("Unknown algorithm $alg"))
+    #     end
 
-#         update2D!(nums, num0, H, algorithm!, param_range1, param_range2, p)
-#         # nums = Int.(nums)
-#         return Int.(nums)
-#     elseif rounds == false
-#         num0 = zeros(Float64, Hs)
+    #     update2D!(nums, num0, H, algorithm!, param_range1, param_range2, p)
+    #     # nums = Int.(nums)
+    #     return Int.(nums)
+    # elseif rounds == false
+    #     num0 = zeros(Float64, Hs)
 
-#         if alg == "BerryPhase"
-#             algorithm! = BerryPhase!
-#         elseif alg == "Z2"
-#             algorithm! = Z2Phase!
-#             nums = zeros(Float64, Hs ÷ 2, size(param_range1, 1), size(param_range2, 1))
-#             num0 = zeros(Float64, Hs ÷ 2)
-#         elseif alg == "Chern"
-#             algorithm! = ChernPhase!
-#         else
-#             throw(ArgumentError("Unknown algorithm $alg"))
-#         end
+    #     if alg == "BerryPhase"
+    #         algorithm! = BerryPhase!
+    #     elseif alg == "Z2"
+    #         algorithm! = Z2Phase!
+    #         nums = zeros(Float64, Hs ÷ 2, size(param_range1, 1), size(param_range2, 1))
+    #         num0 = zeros(Float64, Hs ÷ 2)
+    #     elseif alg == "Chern"
+    #         algorithm! = ChernPhase!
+    #     else
+    #         throw(ArgumentError("Unknown algorithm $alg"))
+    #     end
 
-#         update2D!(nums, num0, H, algorithm!, param_range1, param_range2, p)
-#         return nums
-#     end
-# end
+    #     update2D!(nums, num0, H, algorithm!, param_range1, param_range2, p)
+    #     return nums
+    # end
+    
+    if rounds == true
+
+        if alg == "BerryPhase"
+            algorithm! = BerryPhase!
+        elseif alg == "Z2"
+            algorithm! = Z2Phase!
+            nums = zeros(Float64, Hs ÷ 2, size(param_range1, 1), size(param_range2, 1))
+            num0 = zeros(Float64, Hs ÷ 2)
+        elseif alg == "Chern"
+            algorithm! = ChernPhase!
+        else
+            throw(ArgumentError("Unknown algorithm $alg"))
+        end
+
+        update2D!(nums, num0, H, algorithm!, param_range1, param_range2, p)
+        nums = round.(Int, nums)
+    elseif rounds == false
+
+        if alg == "BerryPhase"
+            algorithm! = BerryPhase!
+        elseif alg == "Z2"
+            algorithm! = Z2Phase!
+            nums = zeros(Float64, Hs ÷ 2, size(param_range1, 1), size(param_range2, 1))
+            num0 = zeros(Float64, Hs ÷ 2)
+        elseif alg == "Chern"
+            algorithm! = ChernPhase!
+        else
+            throw(ArgumentError("Unknown algorithm $alg"))
+        end
+
+        update2D!(nums, num0, H, algorithm!, param_range1, param_range2, p)
+    end
+end
 
 @doc raw"""
     calcPhaseDiagram(H::Function, param_range::T, alg::String; N::Int=51, gapless::Real=0.0, rounds::Bool=true, plot::Bool=false) where {T<:AbstractVector}
@@ -223,10 +257,10 @@ function calcPhaseDiagram(H::Function, param_range1::T1, param_range2::T2, alg::
 
     p = Params(; Hamiltonian, dim, N, gapless, rounds, Hs)
 
-    nums = zeros(Float64, Hs, size(param_range1, 1), size(param_range2, 1))
-    num0 = zeros(Float64, Hs)
+    # nums = zeros(Float64, Hs, size(param_range1, 1), size(param_range2, 1))
+    # num0 = zeros(Float64, Hs)
 
-    # nums = calc_data2D(H, param_range1, param_range2, alg, p)
+    nums = calc_data2D(H, param_range1, param_range2, alg, p)
 
     if rounds == true
 
