38a0d2bfa93c1f7e33fe6aad1a0ba4988200b947
Keisuke Adachi <18s2002x@gmail.com>
2023-10-21 22:44:20 +0900

    add calcChernSurface

--- src/calcChernSurface.jl
+++ src/calcChernSurface.jl
@@ -26,30 +26,29 @@ function numsk3(H, kn_range, N, gapless, rounds, Hs)
 end
 
 @doc raw"""
+    calcChernSurface(H::Function, kn::String, kn_range::T; N::Int=51, gapless::Real=0.0, rounds::Bool=true, plot::Bool=false) where {T<:AbstractVector}
+
+ Arguments
+ - Hamiltionian::Function: The Hamiltonian matrix with three-dimensional wavenumber `k` as an argument.
+ - kn::String: Compute the Chern number of the plane perpendicular to the `"kn"` direction in Brillouin zone (`"k1"`, `"k2"`, `"k3"`).
+ - kn_range::T: Mesh in `"kn"` direction.
+ - N::Int=51: The number of meshes when discretizing the Brillouin Zone. It is preferable for `N` to be an odd number to increase the accuracy of the calculation.
+ - gapless::Real: The threshold that determines the state to be degenerate. Coarsening the mesh(`N`) but increasing `gapless` will increase the accuracy of the calculation.
+ - rounds::Bool=true: An option to round the value of the topological number to an integer value. The topological number returns a value of type `Int` when `true`, and a value of type `Float` when `false`.
 """
 function calcChernSurface(H::Function, kn::String, kn_range::T; N::Int=51, gapless::Real=0.0, rounds::Bool=true, plot::Bool=false) where {T<:AbstractVector}
     
     Hs = size(H(zeros(3)), 1)
 
     if kn == "k1"
-        # k0(k, p) = [p, k[1], k[2]]
         nums = numsk1(H, kn_range, N, gapless, rounds, Hs)
     elseif kn == "k2"
-        # k0(k, p) = [k[1], p, k[2]]
         nums = numsk2(H, kn_range, N, gapless, rounds, Hs)
     elseif kn == "k3"
-        # k0(k, p) = [k[1], k[2], p]
         nums = numsk3(H, kn_range, N, gapless, rounds, Hs)
     else
         throw(ArgumentError("Unknown keyword $kn"))
     end
-    
-    # H0(k, p) = H(k0(k, p))
-    # Hamiltonian(k) = H0(k, 0.0)
-
-    # param = Params(; Hamiltonian, dim=2, N, gapless, rounds, Hs)
-
-    # nums = calc_data1D(H0, kn_range, "Chern", param)
     
     if plot == true
         plot1D(nums, param_range)
