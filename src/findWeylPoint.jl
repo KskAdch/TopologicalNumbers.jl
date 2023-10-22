f350eab6b44f261be710e502a64fffc12906d727
Keisuke Adachi <18s2002x@gmail.com>
2023-10-22 22:50:12 +0900

    add findWeylPoint

--- src/findWeylPoint.jl
+++ src/findWeylPoint.jl
@@ -2,4 +2,8 @@
     findWeylPoint(Hamiltonian::Function)
 """
 function findWeylPoint(Hamiltonian::Function)
+
+    E(k) = eigvals(Hamiltonian(k))
+    jacobE(k) = jacobian(E, [k[1], k[2], k[3]])[1]
+
 end
