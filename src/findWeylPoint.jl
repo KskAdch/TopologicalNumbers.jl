7b2c52217e80c2460762f3f03849570845bea22e
Keisuke Adachi <18s2002x@gmail.com>
2023-10-26 00:39:33 +0900

    findWeylPoint

--- src/findWeylPoint.jl
+++ src/findWeylPoint.jl
@@ -6,18 +6,23 @@ function findWeylPoint(Hamiltonian::Function; N::Int=51)
     E(k) = eigvals(Hamiltonian(k))
     jacobE(k) = jacobian(E, [k[1], k[2], k[3]])[1]
 
-    Ene = zeros(N, N, N, Hs)
-    ene = zeros(Hs, 3)
+    Ene = zeros(Hs, 3)
     k = zeros(3)
-    for l in 1:N
-        k3 = 2pi*(l-1) / N
+    k_list = 
+    n = 0
+    for i in 1:N
+        k[1] = 2pi*(i-1) / N
         for j in 1:N
-            k2 = 2pi*(j-1) / N
-            for i in 1:N, 
-                k1 = 2pi*(i-1) / N
-                k .= [k1, k2, k3]
-                ene .= jacobE(k)
-                Ene[i, j, l, :] .= [norm(ene[i, :], 2) for i in 1:Hs]
+            k[2] = 2pi*(j-1) / N
+            for l in 1:N, 
+                k[3] = 2pi*(l-1) / N
+                Ene .= jacobE(k)
+                if abs(Ene[1, 1]) < 1e-1 && abs(Ene[1, 2]) < 1e-1 && abs(Ene[1, 3]) < 1e-1 && n == 0
+                    k0_list = k
+                    n = 1
+                elseif abs(Ene[1, 1]) < 1e-1 && abs(Ene[1, 2]) < 1e-1 && abs(Ene[1, 3]) < 1e-1
+                    k0_list = [k0_list k]
+                end
             end
         end
     end
