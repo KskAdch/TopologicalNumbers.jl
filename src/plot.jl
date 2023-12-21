1ef3476f76791dfe337d4d266bd331c3de90a028
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2023-12-21 21:49:57 +0900

    update backend

--- src/plot.jl
+++ src/plot.jl
@@ -1,25 +1,33 @@
+function marker(i)
+    markers = ("o", "v", "^", ",", "<", ">", "8", "s", "*", "h", "H", "D", "d", ".", "1", "2", "3", "4", "+", "x", "|", "_", "p")
+    if i > length(markers)
+        i = i % length(markers)
+    end
+    return markers[i]
+end
+
 @doc raw"""
     plot1D(nums::T1, param_range::T2; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram") where {T1<:AbstractMatrix,T2<:AbstractVector}
 """
 function plot1D(nums::T1, param_range::T2; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram") where {T1<:AbstractMatrix,T2<:AbstractVector}
 
     fig = figure()
+    ax = fig.add_subplot(111)
 
     if labels == true
-        ax = Axis(fig[1, 1], xminorticksvisible=true, xminorgridvisible=true, xminorticks=IntervalsBetween(2), xlabel="p", ylabel="ν")
-    else
-        ax = Axis(fig[1, 1], xlabelvisible=false, ylabelvisible=false)
+        ax.set_xlabel(L"p")
+        ax.set_ylabel(L"\nu")
     end
+    ax.grid()
+    ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=true))
 
-
-    for i in 1:size(nums, 2)
-        scatter!(ax, param_range, nums[:, i], label="Band$(i)", markersize=15)
+    for i in axes(nums, 2)
+        ax.scatter(param_range, nums[:, i], marker=marker(i), label="Band$(i)")
     end
-    Legend(fig[1, 2], ax)
+    ax.legend()
 
     p = (; disp, png, pdf, svg, filename)
     output(fig, p)
-    # display(fig)
     fig
 end
 
@@ -29,22 +37,23 @@ end
 function plot1D(result::NamedTuple; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram")
 
     fig = figure()
+    ax = fig.add_subplot(111)
 
     if labels == true
-        ax = Axis(fig[1, 1], xminorticksvisible=true, xminorgridvisible=true, xminorticks=IntervalsBetween(2), xlabel="p", ylabel="ν")
-    else
-        ax = Axis(fig[1, 1], xlabelvisible=false, ylabelvisible=false)
+        ax.set_xlabel(L"p")
+        ax.set_ylabel(L"\nu")
     end
+    ax.grid()
+    ax.yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=true))
 
 
-    for i in 1:size(result.nums, 2)
-        scatter!(ax, result.param, result.nums[:, i], label="Band$(i)", markersize=15)
+    for i in axes(result.nums, 2)
+        ax.scatter(result.param, result.nums[:, i], marker=marker(i), label="Band$(i)")
     end
-    Legend(fig[1, 2], ax)
+    ax.legend()
 
     p = (; disp, png, pdf, svg, filename)
     output(fig, p)
-    # display(fig)
     fig
 end
 
@@ -64,14 +73,8 @@ function plot2D(nums::T1, param_range1::T2, param_range2::T3; labels::Bool=true,
     im = ax.imshow(nums, cmap="jet", interpolation="none", origin="lower", extent=(param_range1[1], param_range1[end], param_range2[1], param_range2[end]), aspect="auto")
     fig.colorbar(im, ax=ax, ticks=matplotlib.ticker.MaxNLocator(integer=true))
 
-
-    # hm = heatmap!(ax, param_range1, param_range2, nums, colormap=:jet1)
-    # ax.aspect = AxisAspect(1)
-    # Colorbar(fig[1, 2], hm)
-
     p = (; disp, png, pdf, svg, filename)
     output(fig, p)
-    # display(fig)
     fig
 end
 
@@ -93,13 +96,9 @@ function plot2D(result::NamedTuple; labels::Bool=true, disp::Bool=true, png::Boo
     im = ax.imshow(nums_half, cmap="jet", interpolation="none", origin="lower", extent=(result.param1[1], result.param1[end], result.param2[1], result.param2[end]), aspect="auto")
     fig.colorbar(im, ax=ax, ticks=matplotlib.ticker.MaxNLocator(integer=true))
 
-    # hm = heatmap!(ax, result.param1, result.param2, nums_half, colormap=:jet1)
-    # ax.aspect = AxisAspect(1)
-    # Colorbar(fig[1, 2], hm)
 
     p = (; disp, png, pdf, svg, filename)
     output(fig, p)
-    # display(fig)
     fig
 end
 
