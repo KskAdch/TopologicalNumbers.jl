097b76b8534107663e7bdc238dc1a71d5904ba64
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2023-12-30 19:01:15 +0900

    add parallel environment

--- src/calcChernSurface.jl
+++ src/calcChernSurface.jl
@@ -1,28 +1,28 @@
-function nums_k1(H, kn_range, N, gapless, rounds, Hs)
+function nums_k1(H, kn_range, N, gapless, rounds, Hs; parallel::T=UseSingleThread()) where {T<:TopologicalNumbersParallel}
     H0(k, p) = H([p, k[1], k[2]])
     Hamiltonian(k) = H0(k, 0.0)
 
     param = Params(; Hamiltonian, dim=2, N, gapless, rounds, Hs)
 
-    calc_data1D(H0, kn_range, "Chern", param)
+    calc_data1D(H0, kn_range, "Chern", parallel, param)
 end
 
-function nums_k2(H, kn_range, N, gapless, rounds, Hs)
+function nums_k2(H, kn_range, N, gapless, rounds, Hs; parallel::T=UseSingleThread()) where {T<:TopologicalNumbersParallel}
     H0(k, p) = H([k[1], p, k[2]])
     Hamiltonian(k) = H0(k, 0.0)
 
     param = Params(; Hamiltonian, dim=2, N, gapless, rounds, Hs)
 
-    calc_data1D(H0, kn_range, "Chern", param)
+    calc_data1D(H0, kn_range, "Chern", parallel, param)
 end
 
-function nums_k3(H, kn_range, N, gapless, rounds, Hs)
+function nums_k3(H, kn_range, N, gapless, rounds, Hs; parallel::T=UseSingleThread()) where {T<:TopologicalNumbersParallel}
     H0(k, p) = H([k[1], k[2], p])
     Hamiltonian(k) = H0(k, 0.0)
 
     param = Params(; Hamiltonian, dim=2, N, gapless, rounds, Hs)
 
-    calc_data1D(H0, kn_range, "Chern", param)
+    calc_data1D(H0, kn_range, "Chern", parallel, param)
 end
 
 @doc raw"""
@@ -36,24 +36,24 @@ end
  - gapless::Real: The threshold that determines the state to be degenerate. Coarsening the mesh(`N`) but increasing `gapless` will increase the accuracy of the calculation.
  - rounds::Bool=true: An option to round the value of the topological number to an integer value. The topological number returns a value of type `Int` when `true`, and a value of type `Float` when `false`.
 """
-function calcChernSurface(H::Function, kn::String; kn_mesh::Int=51, N::Int=51, gapless::Real=0.0, rounds::Bool=true, plot::Bool=false)
-    
+function calcChernSurface(H::Function, kn::String; kn_mesh::Int=51, N::Int=51, gapless::Real=0.0, rounds::Bool=true, parallel::T=UseSingleThread(), plot::Bool=false) where {T<:TopologicalNumbersParallel}
+
     Hs = size(H(zeros(3)), 1)
-    kn_range = 2pi*(0:(kn_mesh-1)) / kn_mesh .+ 2pi*1e-5
+    kn_range = 2pi * (0:(kn_mesh-1)) / kn_mesh .+ 2pi * 1e-5
 
     if kn == "k1"
-        nums = nums_k1(H, kn_range, N, gapless, rounds, Hs)
+        nums = nums_k1(H, kn_range, N, gapless, rounds, Hs; parallel)
     elseif kn == "k2"
-        nums = nums_k2(H, kn_range, N, gapless, rounds, Hs)
+        nums = nums_k2(H, kn_range, N, gapless, rounds, Hs; parallel)
     elseif kn == "k3"
-        nums = nums_k3(H, kn_range, N, gapless, rounds, Hs)
+        nums = nums_k3(H, kn_range, N, gapless, rounds, Hs; parallel)
     else
         throw(ArgumentError("Unknown keyword $kn"))
     end
-    
+
     if plot == true
         plot1D(nums, kn_range)
     end
 
-    (; kn, param = kn_range, nums)
+    (; kn, param=kn_range, nums)
 end
