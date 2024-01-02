97858a3acab41e96beb7022b2d1c7539a34c1c8f
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2024-01-03 08:15:05 +0900

    Bug fixes

--- src/problems.jl
+++ src/problems.jl
@@ -1,35 +1,35 @@
 abstract type TopologicalNumbersProblems end
 
 # Problem for calculating the first Chern number
-@kwdef struct BPProblem <: TopologicalNumbersProblems
-    H
+@kwdef struct BPProblem{T1<:Function,T2<:Union{Tuple,AbstractVector,Int},T3<:Real,T4<:Bool} <: TopologicalNumbersProblems
+    H::T1
     N::T2 = 51
-    gapless = 0.0
-    rounds = true
+    gapless::T3 = 0.0
+    rounds::T4 = true
 end
 
 # Problem for calculating the first Chern number
-@kwdef struct FCProblem <: TopologicalNumbersProblems
-    H
+@kwdef struct FCProblem{T1<:Function,T2<:Union{Tuple,AbstractVector,Int},T3<:Real,T4<:Bool} <: TopologicalNumbersProblems
+    H::T1
     N::T2 = 51
-    gapless = 0.0
-    rounds = true
+    gapless::T3 = 0.0
+    rounds::T4 = true
 end
 
 # Problem for calculating the second Chern number
-struct SCProblem <: TopologicalNumbersProblems
-    H
+@kwdef struct SCProblem{T1<:Function,T2<:Union{Tuple,AbstractVector,Int},T3<:Union{Real,Nothing},T4<:Bool} <: TopologicalNumbersProblems
+    H::T1
     N::T2 = 30
-    Nfill = nothing
-    RV = true
+    Nfill::T3 = nothing
+    RV::T4 = true
 end
 
 # Problem for calculating the Z2 invariant
-struct Z2Problem <: TopologicalNumbersProblems
-    H
+@kwdef struct Z2Problem{T1<:Function,T2<:Union{Tuple,AbstractVector,Int},T3<:Bool} <: TopologicalNumbersProblems
+    H::T1
     N::T2 = 50
-    rounds = true
-    TR = false
+    rounds::T3 = true
+    TR::T3 = false
 end
 
 
@@ -37,25 +37,25 @@ end
 abstract type TopologicalNumbersSolutions end
 
 # Solution for calculating the first Chern number
-@kwdef struct BPSolution <: TopologicalNumbersSolutions
-    TopologicalNumber = nothing
-    Total = nothing
+@kwdef struct BPSolution{T} <: TopologicalNumbersSolutions
+    TopologicalNumber::T = nothing
+    Total::T = nothing
 end
 
 # Solution for calculating the first Chern number
-@kwdef struct FCSolution <: TopologicalNumbersSolutions
-    TopologicalNumber = nothing
-    Total = nothing
+@kwdef struct FCSolution{T} <: TopologicalNumbersSolutions
+    TopologicalNumber::T = nothing
+    Total::T = nothing
 end
 
 # Solution for calculating the second Chern number
-struct SCSolution <: TopologicalNumbersSolutions
-    TopologicalNumber = nothing
+@kwdef struct SCSolution{T} <: TopologicalNumbersSolutions
+    TopologicalNumber::T = nothing
 end
 
 # Solution for calculating the Z2 invariant
-struct Z2Solution <: TopologicalNumbersSolutions
-    TopologicalNumber = nothing
-    TRTopologicalNumber = nothing
-    Total = nothing
+@kwdef struct Z2Solution{T} <: TopologicalNumbersSolutions
+    TopologicalNumber::T = nothing
+    TRTopologicalNumber::T = nothing
+    Total::T = nothing
 end
