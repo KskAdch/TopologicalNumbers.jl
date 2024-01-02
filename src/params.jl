4e682ea0e6402e3aadede75754951bfbceb95cac
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2024-01-03 07:40:08 +0900

    Update Params struct in src/params.jl

--- src/params.jl
+++ src/params.jl
@@ -1,16 +1,17 @@
 
-@with_kw mutable struct Params{T1<:Int,T2<:Union{Int,Tuple,AbstractVector},T3<:AbstractFloat,T4<:Bool}
-    Hamiltonian::Function
+@kwdef struct Params{F,T1<:Int,T2<:Union{Int,Tuple,AbstractVector},T3<:AbstractFloat,T4<:Bool}
+    Ham::F
     dim::T1
-    Nfill::T1 = 0
+    Nfill::T1 = 1
     Hs::T1
     N::T2
-    gapless::T3
-    rounds::T4
+    gapless::T3 = 0.0
+    rounds::T4 = true
+    returnRealValue::T4 = true
 end
 
 # Define a mutable struct named TemporalSecondChern with multiple type parameters
-@with_kw mutable struct TemporalSecondChern{T1,T2,T3,T4,T5,T6}
+@kwdef mutable struct TemporalSecondChern{T1,T2,T3,T4,T5,T6}
     chern::T1      # Second Chern number
     k::T2          # Momentum vector
     evec::T3       # Eigenvectors
