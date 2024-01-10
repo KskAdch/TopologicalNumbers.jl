84fe9bfbc6866863bdf2e420ede6559a29b74162
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2024-01-10 22:51:20 +0900

    Add document

--- src/plot.jl
+++ src/plot.jl
@@ -8,6 +8,27 @@ end
 
 @doc raw"""
     plot1D(::T1, nums::T2, param_range::T3; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram") where {T1<:SecondChernAlgorithms,T2<:AbstractVector,T3<:AbstractVector}
+    
+Plot a 1D phase diagram.
+
+# Arguments
+- `::T1`: An object of type `T1` that implements the `SecondChernAlgorithms` interface.
+- `nums::T2`: A vector of numbers representing the values to be plotted.
+- `param_range::T3`: A vector representing the parameter range.
+- `labels::Bool=true`: Whether to display labels on the plot. Default is `true`.
+- `disp::Bool=true`: Whether to display the plot. Default is `true`.
+- `png::Bool=false`: Whether to save the plot as a PNG file. Default is `false`.
+- `pdf::Bool=false`: Whether to save the plot as a PDF file. Default is `false`.
+- `svg::Bool=false`: Whether to save the plot as an SVG file. Default is `false`.
+- `filename::String="phaseDiagram"`: The filename for the saved plot. Default is `"phaseDiagram"`.
+
+# Examples
+```julia
+plot1D(obj, nums, param_range)
+```
+
+# Returns
+- `fig`: The matplotlib figure object representing the plot.
 """
 function plot1D(
     ::T1,
@@ -43,6 +64,28 @@ end
 
 @doc raw"""
     plot1D(nums::T1, param_range::T2; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram") where {T1<:AbstractMatrix,T2<:AbstractVector}
+    
+Plot a 1D phase diagram.
+
+# Arguments
+- `nums::T1`: A matrix or vector containing the data to be plotted.
+- `param_range::T2`: A vector representing the parameter range.
+- `labels::Bool=true`: Whether to display axis labels. Default is `true`.
+- `disp::Bool=true`: Whether to display the plot. Default is `true`.
+- `png::Bool=false`: Whether to save the plot as a PNG file. Default is `false`.
+- `pdf::Bool=false`: Whether to save the plot as a PDF file. Default is `false`.
+- `svg::Bool=false`: Whether to save the plot as an SVG file. Default is `false`.
+- `filename::String="phaseDiagram"`: The filename for the saved plot. Default is "phaseDiagram".
+
+# Examples
+```julia
+nums = [1 2 3; 4 5 6]
+param_range = [0.1, 0.2, 0.3]
+plot1D(nums, param_range)
+```
+
+# Returns
+- `fig`: The matplotlib figure object representing the plot.
 """
 function plot1D(
     nums::T1,
@@ -83,6 +126,25 @@ end
 
 @doc raw"""
     plot1D(result::NamedTuple; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram")
+    
+Plot a 1D phase diagram.
+
+# Arguments
+- `result::NamedTuple`: A named tuple containing the result data.
+- `labels::Bool=true`: Whether to display axis labels.
+- `disp::Bool=true`: Whether to display the plot.
+- `png::Bool=false`: Whether to save the plot as a PNG file.
+- `pdf::Bool=false`: Whether to save the plot as a PDF file.
+- `svg::Bool=false`: Whether to save the plot as an SVG file.
+- `filename::String="phaseDiagram"`: The filename for the saved plot.
+
+# Returns
+- `fig`: The matplotlib figure object.
+
+# Example
+```julia
+julia>
+```
 """
 function plot1D(
     result::NamedTuple;
@@ -120,9 +182,29 @@ function plot1D(
     fig
 end
 
-
 @doc raw"""
     plot2D(nums::T1, param_range1::T2, param_range2::T3; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram") where {T1<:AbstractArray,T2<:AbstractVector,T3<:AbstractVector}
+
+Plot a 2D phase diagram.
+
+# Arguments
+- `nums::T1`: An array of numbers representing the phase diagram.
+- `param_range1::T2`: A vector representing the range of the first parameter.
+- `param_range2::T3`: A vector representing the range of the second parameter.
+- `labels::Bool=true`: Whether to display axis labels. Default is `true`.
+- `disp::Bool=true`: Whether to display the plot. Default is `true`.
+- `png::Bool=false`: Whether to save the plot as a PNG file. Default is `false`.
+- `pdf::Bool=false`: Whether to save the plot as a PDF file. Default is `false`.
+- `svg::Bool=false`: Whether to save the plot as an SVG file. Default is `false`.
+- `filename::String="phaseDiagram"`: The filename for the saved plot. Default is "phaseDiagram".
+
+# Returns
+- `fig`: The matplotlib figure object.
+
+# Example
+```julia
+julia>
+```
 """
 function plot2D(
     nums::T1,
@@ -156,6 +238,25 @@ end
 
 @doc raw"""
     plot2D(result::NamedTuple; labels::Bool=true, disp::Bool=true, png::Bool=false, pdf::Bool=false, svg::Bool=false, filename::String="phaseDiagram")
+    
+Plot a 2D phase diagram.
+
+# Arguments
+- `result::NamedTuple`: A named tuple containing the result data.
+- `labels::Bool=true`: Whether to display axis labels.
+- `disp::Bool=true`: Whether to display the plot.
+- `png::Bool=false`: Whether to save the plot as a PNG file.
+- `pdf::Bool=false`: Whether to save the plot as a PDF file.
+- `svg::Bool=false`: Whether to save the plot as an SVG file.
+- `filename::String="phaseDiagram"`: The filename for the saved plot.
+
+# Returns
+- `fig`: The matplotlib figure object.
+
+# Example
+```julia
+julia>
+```
 """
 function plot2D(
     result::NamedTuple;
