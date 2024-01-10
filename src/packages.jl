944430b53551bd8b83be787a691c5fd5d19ab90c
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2024-01-10 23:25:36 +0900

    Add CondaPkg and PythonCall dependencies
    Import pfaffian and numpy libraries

--- src/packages.jl
+++ src/packages.jl
@@ -4,5 +4,9 @@ using LaTeXStrings
 using LinearAlgebra
 using Parameters
 using ProgressBars
+using PythonCall
 using PythonPlot
 using StaticArrays
+
+const pf = pyimport("pfapack.pfaffian")
+const np = pyimport("numpy")
