72a64afea4d0123a3d8f87a17781b3965fc28868
Keisuke Adachi <139373570+KskAdch@users.noreply.github.com>
2024-01-16 16:06:48 +0900

    add ThoulessPump

--- src/models.jl
+++ src/models.jl
@@ -264,18 +264,69 @@ Hamiltonian of the Thouless pumping model.
      ThoulessPump(k::T1, p::T2) where {T1<:Union{AbstractVector,Tuple}, T2<:Union{AbstractVector,Tuple}}
 
 # Arguments
- - `k::T1`: two-dimensional wavenumber `k`.
+ - `k::T1`: when one-dimensional wavenumber `k₁` and time `t`, `k=[k₁, t]`.
  - `p::T2`: parameters defined as below.
 
 
 # Definition
- Hamiltonian of the Thouless pumping model is defined as
+ Hamiltonian of the Kane--Mele model is defined as
 ```math
-H(k)=
+H(k)=\begin{pmatrix}
+    h_{3}(\bm{k})                 & h_{5}(\bm{k})-ih_{4}(\bm{k}) & 0                            & -h_{5}(\bm{k})-ih_{1}(\bm{k}) \\
+    h_{5}(\bm{k})+ih_{4}(\bm{k})  & -h_{3}(\bm{k})               & h_{5}(\bm{k})-ih_{1}(\bm{k}) & 0                             \\
+    0                             & h_{5}(\bm{k})+ih_{1}(\bm{k}) & -h_{3}(\bm{k})               & h_{5}(\bm{k})-ih_{4}(\bm{k})  \\
+    -h_{5}(\bm{k})+ih_{1}(\bm{k}) & 0                            & h_{5}(\bm{k})+ih_{4}(\bm{k}) & h_{3}(\bm{k})
+\end{pmatrix}
 ```
-where,,,
+
+```math
+\begin{align*}
+     h_{1}(\bm{k})=&-\Delta\sin(k_{1}) \\
+     h_{2}(\bm{k})=&-\Delta(1-\cos(k_{1})) \\
+     h_{3}(\bm{k})=&m_{0}\sin(t) \\
+     h_{4}(\bm{k})=&-t_{0}(1+\cos(t))\sin(k_{1}) \\
+     h_{5}(\bm{k})=&-t_{0}(1-\cos(t))-t_{0}(1+\cos(t)))\cos(k_{1})
+\end{align*}
+```
+where $t_{0}$ is the amplitude of the nearest neighbor hopping in the tight binding model.
+$m_{0}$ is the magnitude of the alternating magnetic field.
+$\Delta$ is the magnitude of spin flip interaction.
+Nondimensionalize with $t_{0}=1$ and $m_{0},\Delta=$`p`.
+
+# Examples
+
+```julia
+julia> ThoulessPump([0, π/3], (-1.0, 0.5))
+4×4 Matrix{ComplexF64}:
+ -0.866025-0.0im      -2.0+0.0im      -0.0-0.0im        0.0+0.0im
+      -2.0-0.0im  0.866025-0.0im      -0.0+0.0im       -0.0-0.0im
+      -0.0-0.0im      -0.0-0.0im  0.866025-0.0im       -2.0+0.0im
+       0.0-0.0im      -0.0-0.0im      -2.0-0.0im  -0.866025-0.0im
+```
 """
 function ThoulessPump(k::T1, p::T2) where {T1<:Union{AbstractVector,Tuple},T2<:Union{AbstractVector,Tuple}}
+    k1, t = k
+    t₀ = 1
+    m₀, Δ = p
+
+    R1 = -Δ*sin(k1)
+    R2 = -Δ*(1-cos(k1))
+    R3 = m₀*sin(t)
+    R4 = -t₀*(1+cos(t))*sin(k1)
+    R0 = -t₀*(1-cos(t))-t₀*(1+cos(t))*cos(k1)
+
+    s0 = [1 0; 0 1]
+    sx = [0 1; 1 0]
+    sy = [0 -im; im 0]
+    sz = [1 0; 0 -1]
+
+    a1 = kron(sy, sx)
+    a2 = kron(sy, sy)
+    a3 = kron(sz, sz)
+    a4 = kron(s0, sy)
+    a0 = kron(s0, sx)
+
+    R1 .* a1 .+ R2 .* a2 .+ R3 .* a3 .+ R4 .* a4 .+ R0 .* a0
 end
 
 @doc raw"""
