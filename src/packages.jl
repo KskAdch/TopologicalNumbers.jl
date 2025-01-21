8a0efb15150821c7bf539f259d5f58721aa1f3f5
Minoru Kanega <phys_chibaraki@yahoo.co.jp>
2025-01-21 16:30:09 +0900

    Enable Agg backend for PythonPlot in AutoMerge CI environment

--- src/packages.jl
+++ src/packages.jl
@@ -4,6 +4,11 @@ using LaTeXStrings
 using LinearAlgebra
 using Parameters: @unpack
 using ProgressBars
-using PythonPlot
 using SparseArrays
 using StaticArrays
+
+# For AutoMerge CI
+if haskey(ENV, "GITHUB_ACTIONS") && haskey(ENV, "AUTOMERGE_GITHUB_TOKEN")
+    ENV["MPLBACKEND"] = "Agg"
+end
+using PythonPlot
